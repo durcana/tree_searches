29ee844b16bc89f3fa888f791c42aa2b4970283d
Amy Durcan <Amy@Amys-MacBook-Air.local>
2016-02-18 11:33:16 -0500

    Remove path, switch print order

--- dfs.py
+++ dfs.py
@@ -7,35 +7,29 @@ family_tree = {'root': ['child1', 'child2'],
 
 
 # depth first search
-def dfs(node, tree, start, path):
-    if path is None:
-        path = [start]
+def dfs(node, tree, start):
     if start == node:
         return node
     for child in tree[start]:
-        if node == child:
-            return node
-        elif child in tree.keys():
-            return dfs(node, tree, child, path + [next])
+        if child in tree.keys():
+            return dfs(node, tree, child)
     return node + ' is not in this tree'
 
 
 # breadth first search
-def bfs(node, tree, start, path):
-    if path is None:
-        path = [start]
+def bfs(node, tree, start):
     if start == node:
         return node
     if node in tree[start]:
         return node
     for child in tree[start]:
         if child in tree.keys():
-            return bfs(node, tree, child, path + [next])
+            return bfs(node, tree, child)
     return node + ' is not in this tree'
 
-import pdb; pdb.set_trace()
-print(bfs('ggc1', family_tree, 'root', path=None))
 # import pdb; pdb.set_trace()
-print(dfs('ggc2', family_tree, 'root', path=None))
+print(dfs('ggc1', family_tree, 'root'))
+# import pdb; pdb.set_trace()
+print(bfs('ggc2', family_tree, 'root'))
 
 # http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
